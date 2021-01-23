b596c8dc2cd5263891db1c0516ea99fc13877b29
Yabl04ko23 <aloshanizi24@gmail.com>
2021-01-23 11:59:06 +0200

    сделал базовый функционал bootstrap

---
+++ src/components/Navigation.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Styles = styled.div`
+  a,
+  .navbar-brend,
+  .navbar-nav .nav-link {
+    color: red;
+    &:hover {
+      color: black;
+    }
+  }
+`;
+
+function Navigation() {
+  return (
+    <>
+      <Styles>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>Logo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse>
+              <Nav>
+                <Nav.Link>
+                  <Link to="/">Home</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/about">About</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/contacts">Contacts</Link>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Styles>
+    </>
+  );
+}
+
+export default Navigation;
